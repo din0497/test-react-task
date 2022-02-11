8345b7f54fef87d028edd6f68cb25afd6d7e0738
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-02-11 23:57:41 +0500

    userProfile

--- src/Components/User/UserProfile.js
+++ src/Components/User/UserProfile.js
@@ -1,11 +1,66 @@
-import Content from '../../Layouts/Content'
-import { Img } from '../../Styles/mainStyles'
-import user from '../../Assets/user.png'
+import Content from "../../Layouts/Content";
+import {
+  BoldText,
+  IconContainer,
+  Img,
+  ImgContainer,
+  PlainText,
+  TextWrapper,
+  Wrapper,
+} from "../../Styles/mainStyles";
+import user from "../../Assets/user.png";
+import { AiOutlineEdit } from "react-icons/ai";
+import { MdAddAPhoto } from "react-icons/md";
 
 const UserProfile = () => {
-return <Content>
-    <Img src={user}/>
-</Content>
-}
+  return (
+    <Content>
+      <Wrapper>
+        <ImgContainer>
+          <Img src={user} />
+          <IconContainer>
+              <MdAddAPhoto/>
+          </IconContainer>
+        </ImgContainer>
+        <TextWrapper>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              gap: "5px",
+            }}
+          >
+            <BoldText>Name Surname</BoldText>
+            <AiOutlineEdit color="#1666C5" size={20} />
+          </div>
 
-export default UserProfile
+          <PlainText>
+            272610
+            <br />
+            Bachelor of science program
+          </PlainText>
+          <BoldText>ENGNEERING AND MANAGEMENT</BoldText>
+        </TextWrapper>
+      </Wrapper>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "5px",
+          marginTop: "20px",
+        }}
+      >
+        <PlainText>Latest enrolment</PlainText>
+        <BoldText>2021/2022 - Full time</BoldText>
+        <PlainText>3 enrolment, cohort 2013/2022</PlainText>
+        <PlainText>
+          Academic progress status:{" "}
+          <span style={{ color: "green" }}>ACTIVE</span>
+        </PlainText>
+      </div>
+    </Content>
+  );
+};
+
+export default UserProfile;
--- src/Styles/mainStyles.js
+++ src/Styles/mainStyles.js
@@ -5,13 +5,51 @@ export const Container = styled.div`
   height: 300px;
   background: #ffffff;
   box-shadow: 3.75215px 3.75215px 18.7607px rgba(0, 0, 0, 0.25);
+  padding: 30px 20px;
 `;
 
 export const MainContents = styled.main`
   display: grid;
   grid-template-columns: auto auto;
-  row-gap: 20px;
   column-gap: 20px;
+  row-gap: 20px;
 `;
 
-export const Img = styled.img``
+export const Img = styled.img`
+  width: 100%;
+  border-radius: 15px;
+`;
+
+export const ImgContainer = styled.div`
+  width: 100px;
+  position: relative;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+`;
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  gap: 7px;
+`;
+
+export const BoldText = styled.h4`
+  text-align: left;
+  margin: 0;
+`;
+export const PlainText = styled.p`
+  color: grey;
+  text-align: left;
+  margin: 0;
+`;
+
+export const IconContainer = styled.div`
+  position: absolute;
+  background: white;
+  right: 0;
+  top: 3;
+`;
